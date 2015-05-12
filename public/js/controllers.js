20d9d7311d0e50349dacbd8bb8192dc0c3a93c45
Richard Lee <rlee46@illinois.edu>
2015-05-12 16:06:13 -0500

    composes queries and sends it to our api

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,27 +1,41 @@
 var papertrail = angular.module('papertrail', ['ngRoute']);
 
 papertrail.controller('SearchController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+
 	$scope.searchForm = {
-		query: "",
-		classifier: "body_bm25",
-		limit: 10
-	};
+        query: "",
+        classifier: "body_bm25",
+        limit: 10
+    };
 
 	$scope.query = function() {
 		// make api call to query and get the results
 		console.log("query() has been called");
 		console.log("The form is", $scope.searchForm);
-		// var queryObject = constructQuery($scope.searchForm);
-		// $http.post('/api/query', queryObject)
-		// 		.success(function(res) {
-		// 			console.log(res);
-		// 		})
-		// 		.error(function(err) {
-		// 			console.log(err);
-		// 		});
+		var queryObject = constructQuery($scope.searchForm);
+        console.log("query obj = ", queryObject)
+		$http.post('/api/query', queryObject)
+		 		.success(function(res) {
+		 			console.log(res);
+		 		})
+		 		.error(function(err) {
+		 			console.log(err);
+		 		});
 	};
 
 	function constructQuery(queryForm) {
-
+        //guessing queryForm is the query they want
+        //and we make a object in the format for the 
+        //btw this only does bm25 queries
+        console.log("query form is ", queryForm)
+        var constructedQuery = {
+            "query" : {
+                "match" : {
+                    //queryForm.classifier: queryForm.query
+                    "body_bm25": queryForm.query
+                }
+            }
+        };
+        return constructedQuery
 	}
 }]);
