7721ee0920c70b846721dd4ceef9056523c97db5
EngrishMajor <ken.yht@gmail.com>
2015-05-12 15:33:59 -0500

    setting up front end querying

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,20 +3,22 @@ var papertrail = angular.module('papertrail', ['ngRoute']);
 papertrail.controller('SearchController', ['$scope', '$http', '$location', function($scope, $http, $location) {
 	$scope.searchForm = {
 		query: "",
-		classifier: "",
+		classifier: "body_bm25",
 		limit: 10
 	};
 
 	$scope.query = function() {
 		// make api call to query and get the results
-		var queryObject = constructQuery($scope.searchForm);
-		$http.post('/api/query', queryObject)
-				.success(function(res) {
-
-				})
-				.error(function(err) {
-
-				});
+		console.log("query() has been called");
+		console.log("The form is", $scope.searchForm);
+		// var queryObject = constructQuery($scope.searchForm);
+		// $http.post('/api/query', queryObject)
+		// 		.success(function(res) {
+		// 			console.log(res);
+		// 		})
+		// 		.error(function(err) {
+		// 			console.log(err);
+		// 		});
 	};
 
 	function constructQuery(queryForm) {
